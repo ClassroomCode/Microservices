0e8bdaf5e6674f23a89dc2c2f2a3847fe30f16d8
Jason Bell <jbell@accelebrate.com>
2024-03-25 13:03:53 -0700

    C# stuff

---
+++ LangDemo/LangDemo/Person.cs
@@ -0,0 +1,15 @@
+﻿
+record Person(string FirstName, string LastName)
+{
+    //
+}
+
+/*
+class Person
+{
+    public required int Id { get; init; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+}
+*/
---
+++ LangDemo/LangDemo/Program.cs
@@ -0,0 +1,93 @@
+﻿
+internal class Program
+{
+    static void Main()
+    {
+        var nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 15, 17 };
+
+        // var evens = nums.FindAll(i => i % 2 == 0);
+
+        var evens = nums.AsParallel().Where(i => i % 2 == 0);
+
+        /*
+        foreach (int i in nums) {
+            if (i % 2 == 0) evens.Add(i);
+        }
+        */
+
+        foreach (var n in evens) {
+            Console.WriteLine(n);
+        }
+
+        Console.WriteLine("===========");
+
+        nums.Add(12);
+
+        foreach (var n in evens) {
+            Console.WriteLine(n);
+        }
+
+        /*
+        var people = new List<Person> {
+            new Person(FirstName: "Bill", LastName: "Gates"),
+            new Person(FirstName: "Joe", LastName: "Gates"),
+            new Person(FirstName: "Jane", LastName: "Gates")
+        };
+
+        foreach(var p in people) Console.WriteLine($"{p.FirstName} {p.LastName}");
+
+        Console.WriteLine("==================");
+
+        var newPeople = people
+            .Where(p => p.FirstName.StartsWith("J"))
+            .Select(p => p with { LastName = "Smith" }
+        );
+
+        foreach (var p in newPeople) Console.WriteLine($"{p.FirstName} {p.LastName}");
+        */
+
+        /*
+        Person p = new Person(FirstName: "Bill", LastName: "Gates");
+        Person p2 = p with { FirstName = "Steve" };
+        */
+
+        /*
+        if (object.ReferenceEquals(p, p2)) {
+            Console.WriteLine("EQUAL");
+        } else {
+            Console.WriteLine("NOT EQUAL");
+        }
+        */
+
+        //Console.WriteLine(p.FirstName);
+
+
+        /*
+        Person p = new Person { FirstName = "Bill", LastName = "Gates" };
+
+        Console.WriteLine(p.FirstName); // Bill
+
+        DoStuff(ref p);
+
+        Console.WriteLine(p.FirstName);  // ?
+        */
+
+
+        //Console.WriteLine("Hello, World!");
+    }
+
+    static void DoStuff(ref Person p) 
+    {
+        //p = new Person { FirstName = "Steve", LastName = "Jobs" };
+    }
+    
+    static bool IsEven(int i)
+    {
+        return i % 2 == 0;
+    }
+}
+
+
+
+
+
