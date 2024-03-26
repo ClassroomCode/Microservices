4b741cf67097372b0aba6c66138b79fc15a3ec41
Jason Bell <jbell@accelebrate.com>
2024-03-26 10:43:10 -0700

    3

--- EComm/ECommService/Data/Repository.cs
+++ EComm/ECommService/Data/Repository.cs
@@ -1,16 +1,24 @@
 ﻿using ECommService.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommService.Data;
 
 public class Repository : IRepository
 {
+    private readonly ECommDbContext _context;
+
+    public Repository(ECommDbContext context)
+    {
+        _context = context;
+    }
+
     public async Task<IEnumerable<Product>> GetAllProductsAsync(CancellationToken token)
     {
-        throw new NotImplementedException();
+        return await _context.Products.ToArrayAsync();
     }
 
     public async Task<Product?> GetProductAsync(int id, CancellationToken token)
     {
-        throw new NotImplementedException();
+        return await _context.Products.SingleOrDefaultAsync(p => p.Id == id);
     }
 }
