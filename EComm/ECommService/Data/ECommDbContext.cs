e23558eafbfcad5118f707bfde39c3bcc071a554
Jason Bell <jbell@accelebrate.com>
2024-03-26 10:21:05 -0700

    2

--- EComm/ECommService/Data/ECommDbContext.cs
+++ EComm/ECommService/Data/ECommDbContext.cs
@@ -11,4 +11,17 @@ public class ECommDbContext : DbContext
     public DbSet<Product> Products { get; set; } = null!;
     public DbSet<Customer> Customers { get; set; } = null!;
     public DbSet<Order> Orders { get; set; } = null!;
+
+    public void AddSeedData()
+    {
+        Products.AddRange(
+            new Product { Id = 1, Name = "Bread", UnitPrice = 3.00, QuantityInStock = 14 },
+            new Product { Id = 2, Name = "Milk", UnitPrice = 4.50, QuantityInStock = 8 },
+            new Product { Id = 3, Name = "Eggs", UnitPrice = 5.00, QuantityInStock = 0 }
+        );
+        Customers.Add(
+            new Customer { Id = 1, Name = "Bill Gates", PostalCode = "23456" }
+        );
+        SaveChanges();
+    }
 }
