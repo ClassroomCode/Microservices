c053eb67321c7e9c57790b5f991bb8689cfc5b1b
Jason Bell <jbell@accelebrate.com>
2024-03-29 06:54:28 -0700

    Docker

--- EComm2/OrderService/RabbitMq/IMessageProducer.cs
+++ EComm2/OrderService/RabbitMq/IMessageProducer.cs
@@ -4,3 +4,6 @@ public interface IMessageProducer
 {
     void SendMessage<T>(T message);
 }
+
+// RabbitMQ Docker container:
+// docker run -d --hostname my-rabbitmq-server --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
